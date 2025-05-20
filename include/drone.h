5e879fbc1dd3cb6a206eba9d86285c6de335e8fa
Bernardusz <b.christian.nugroho.study@gmail.com>
2025-05-20 19:45:49 +0700

    Finished... EVERYTHING, only test drive left

--- include/drone.h
+++ include/drone.h
@@ -1,12 +1,11 @@
 #ifndef DRONE_H
 #define DRONE_H
+#include <Servo.h>
+
+extern Servo motorLeft;
+extern Servo motorRight;
 
 void droneController(char command);
-
-void moveForward();
-void moveBackward();
-void turnLeft();
-void turnRight();
-void stopMotors();
+void initializeDrone();
 
 #endif
