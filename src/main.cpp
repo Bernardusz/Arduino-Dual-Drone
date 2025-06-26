5c9dbb5c126f5259a75f90f8e72c9d6ff959c45f
Bernardusz <b.christian.nugroho.study@gmail.com>
2025-06-26 08:11:11 +0700

    MVP Working version'

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <Arduino.h>
 #include <drone.h>
-#include <sensor.h>
 #include <comm.h>
 float ax, ay, az, gx, gy, gz;
+char command = 'B';
 
 void setup() {
   // put your setup code here, to run once:
-  initializeSensor();
   initializeBluetooth();
   initializeDrone();
   delay(2000);
@@ -15,21 +14,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  readSensor(ax, ay, az, gx, gy, gz);
-  if (gy > 20){
-    droneController('D');
+  if (Serial.available() > 0) {
+    command = readBluetooth();
   }
-  else if (gy < -20){
-    droneController('A');
-  }
-  else {
-    if (Serial.available() > 0) {
-      char command = readBluetooth();
-      droneController(command);
-    }
-    else{
-      droneController('B'); //Sending B as stop motor
-    }
-  }
+  droneController(command);
 }
+
 
